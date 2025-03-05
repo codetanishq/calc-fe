7d056807d34a51177f0a12efa8363a2c68d2cd76
codetanishq <guptasagar4444@gmail.com>
2025-03-05 20:38:57 +0530

    api url update

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -160,6 +160,15 @@ useEffect(() => {
 
     const runRoute = async () => {
         const canvas = canvasRef.current;
+        // if (canvas) {
+        //     const response = await axios({
+        //         method: 'post',
+        //         url: `${import.meta.env.VITE_API_URL}/calculate`,
+        //         data: {
+        //             image: canvas.toDataURL('image/png'),
+        //             dict_of_vars: dictOfVars
+        //         }
+        //     });
     
         if (canvas) {
             const response = await axios({
